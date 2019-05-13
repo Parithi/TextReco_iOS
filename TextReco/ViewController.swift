d0cdedeab9f32e73326ba629144a5cb0eb331591
ElamParithi Arul <mail@parithi.com>
2019-05-12 20:01:27 -0400

    Initial Commit

---
+++ TextReco/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextReco
+//
+//  Created by ElamParithi Arul on 2019-05-12.
+//  Copyright © 2019 Parithi Network. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
